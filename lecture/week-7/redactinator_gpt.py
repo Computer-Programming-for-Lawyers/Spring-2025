f3aef1960dff3cc593976e3cef6847359c3dc79b
Rachel Orey <rachel.orey@gmail.com>
2025-03-30 18:15:42 -0400

    lec7

---
+++ lecture/week-7/redactinator_gpt.py
@@ -0,0 +1,18 @@
+import pypdf
+import os
+import datetime
+
+
+def save_string_to_file(text: str, file_location: str) -> None:
+  """
+  Save a string to a text file.
+
+  Required arguments:
+      text -- The string you would like to save.
+      file_location -- The location of the
+       text file to be output. E.g., 'output.txt'
+       or 'some_folder/output.txt'.
+
+  This function only saves the file; it
+    does not return any output besides None.
+  """
